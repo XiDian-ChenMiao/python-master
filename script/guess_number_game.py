673993d4d197138e89c2952d2be64b95463b19e9
ChenMiao <786078509@qq.com>
2020-07-26 12:01:54 +0800

    modify the guess number game script and check the user input

--- script/guess_number_game.py
+++ script/guess_number_game.py
@@ -7,14 +7,15 @@ import sys
 def guess_num(actual_num):
     step = 1
     while True:
-        guess_num = input('[+] enter number: ')
-        if not isinstance(guess_num, int):
-            print('please input a number.')
+        guess_num = raw_input('[+] enter number: ')
+        if not guess_num.isdigit():
+            print('invalid input, please input a number.')
             continue
-        if guess_num == actual_num:
+        num = int(guess_num)
+        if num == actual_num:
             print('you pass the game by ' + str(step) + ' steps, oh my baby, come on!')
             break
-        elif guess_num > actual_num:
+        elif num > actual_num:
             print('much bigger, try again.')
         else:
             print('too smaller, try again')
